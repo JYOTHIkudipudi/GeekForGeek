7299ccef36825ba1ac9fcd4453d336dae8353029
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2025-05-06 17:35:30 +0530

    MinimumElement.cpp

---
+++ MinimumElement.cpp
@@ -0,0 +1,37 @@
+/*   Minimum element in BST
+
+Given the root of a Binary Search Tree. The task is to find the minimum valued element in this given BST.
+
+Examples
+Input: root = [5, 4, 6, 3, N, N, 7, 1]
+ex-1
+Output: 1
+Input: root = [10, 5, 20, 2]
+ex-2
+Output: 2
+Input: root = [10, N, 10, N, 11]
+             10
+              \
+               10
+                \
+                 11
+Output: 10
+
+Constraints:
+0 <= number of nodes <= 105
+0 <= node->data <= 105   */
+
+class Solution {
+  public:
+    int minValue(Node* root) {
+         if (root == nullptr)
+        return -1;  
+
+    Node* current = root;
+    while (current->left != nullptr) {
+        current = current->left;
+    }
+    return current->data;
+        
+    }
+};
