f42d559fd6a6f61cd3251dc87ffa11f0e24fdc14
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2025-06-07 21:00:43 +0530

    ArrayInsertAtEnd.cpp

--- ArrayInsertAtEnd.cpp
+++ ArrayInsertAtEnd.cpp
@@ -1,7 +1,7 @@
 /*   Array insert at end
 
 Insertion is a basic but frequently used operation. Arrays in most languages can not be dynamically shrinked or expanded. 
-Here, we will work with such arrays and try to insert an element at the end of the array.
+Here, we will work with such arrays and try to insert an element at the end of the array. 
 
 You need to modify the given array arr. The size of the array is given by sizeOfArray. You need to insert an element at the end. 
 Array already have the sizeofarray -1 elements. Find Kth Rotation
