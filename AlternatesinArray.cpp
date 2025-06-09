0161ab9d174171dbdc9cce572179a078e937daaf
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2025-06-09 20:35:20 +0530

    AlternatesinArray.cpp

--- AlternatesinArray.cpp
+++ AlternatesinArray.cpp
@@ -11,7 +11,7 @@ Skip second element: 2
 Take third element: 3
 Skip fourth element: 4
 
-Input: arr[] = [1, 2, 3, 4, 5]
+Input: arr[] = [1, 2, 3, 4, 5] 
 Output: 1 3 5
 Explanation:
 Take first element: 1
