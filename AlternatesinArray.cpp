70d3c002b82f5326d7690e4ad33830907f2287f4
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2025-06-14 20:47:08 +0530

    AlternatesinArray.cpp

--- AlternatesinArray.cpp
+++ AlternatesinArray.cpp
@@ -4,7 +4,7 @@ You are given an array arr[], the task is to return a list elements of arr in al
 
 Examples: 
 Input: arr[] = [1, 2, 3, 4]
-Output: 1 3
+Output: 1 3 
 Explanation: 
 Take first element: 1
 Skip second element: 2
