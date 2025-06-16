cb4df2051146ab4016433403abbc534430f78d35
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2025-06-16 20:13:44 +0530

    AlternatesinArray.cpp

--- AlternatesinArray.cpp
+++ AlternatesinArray.cpp
@@ -2,7 +2,7 @@
 
 You are given an array arr[], the task is to return a list elements of arr in alternate order (starting from index 0).
 
-Examples: 
+Examples:  
 Input: arr[] = [1, 2, 3, 4]
 Output: 1 3 
 Explanation: 
