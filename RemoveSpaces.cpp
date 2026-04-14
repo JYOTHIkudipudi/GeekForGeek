e28586ee711887778fb3e439cd1a8e92aebf740e
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2026-04-14 20:29:55 +0530

    Create RemoveSpaces.cpp

---
+++ RemoveSpaces.cpp
@@ -0,0 +1,31 @@
+/*  Remove Spaces
+
+Given a string s, remove all the spaces from the string and return the modified string.
+
+Examples:
+
+Input: s = "g eeks for ge eks"
+Output: "geeksforgeeks"
+Explanation: All space characters are removed from the given string while preserving the order of the remaining characters, resulting in the final string "geeksforgeeks".
+Input: s = "abc d "
+Output: "abcd"
+Explanation:  All space characters are removed from the given string while preserving the order of the remaining characters, resulting in the final string "abcd".
+Constraints:
+1 ≤ |s| ≤ 105   */
+
+class Solution {
+  public:
+    string removeSpaces(string& s) {
+        // code here
+        string ans = "";
+        
+        for(char c : s){
+            if(c != ' '){
+                ans += c;
+            }
+        }
+        
+        return ans;
+    }
+};
+
