fae40aa66a5eb54d0ff6e4fa7fbaeb35c7b513a7
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2026-06-17 20:57:41 +0530

    Create CutropetoMaximiseProduct.cpp

---
+++ CutropetoMaximiseProduct.cpp
@@ -0,0 +1,31 @@
+/*  Cut rope to maximise product
+Difficulty: MediumAccuracy: 40.16%Submissions: 20K+Points: 4
+Given a rope of length n meters, cut it into multiple smaller ropes such that the product of their lengths is maximized. At least one cut is mandatory.
+
+Examples:
+
+Input: n = 2
+Output: 1
+Explanation: Since 1 cut is mandatory. Maximum obtainable product is 1 * 1 = 1.
+Input: n = 5
+Output: 6
+Explanation: Maximum obtainable product is 2 * 3 = 6.
+Constraints:
+2 ≤ n ≤ 58  */
+
+class Solution {
+  public:
+    int maxProduct(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+
+        int product = 1;
+
+        while (n > 4) {
+            product *= 3;
+            n -= 3;
+        }
+
+        return product * n;
+    }
+};
