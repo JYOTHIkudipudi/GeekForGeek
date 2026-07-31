b3693892190b9f65949e89ce858e137fba685d42
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2026-07-31 15:56:29 +0530

    Update SubSets.cpp

--- SubSets.cpp
+++ SubSets.cpp
@@ -35,70 +35,67 @@ Constraints:
 1 ≤ arr.size() ≤ 105
 1 ≤ arr[i] ≤ 30
  */
-
 class Solution {
 public:
-    static const int MOD = 1e9 + 7;
+    static const int MOD = 1000000007;
 
-    int primeSubsets(vector<int>& arr) {
+    int countSubsets(vector<int> &arr) {
         vector<int> primes = {2,3,5,7,11,13,17,19,23,29};
 
         vector<int> mask(31, -1);
 
-        for(int x=2;x<=30;x++){
-            int t=x;
-            int m=0;
-            bool ok=true;
+        for (int x = 2; x <= 30; x++) {
+            int t = x;
+            int m = 0;
+            bool ok = true;
 
-            for(int i=0;i<10;i++){
-                int p=primes[i];
-                int cnt=0;
-                while(t%p==0){
+            for (int i = 0; i < 10; i++) {
+                int cnt = 0;
+                while (t % primes[i] == 0) {
                     cnt++;
-                    t/=p;
+                    t /= primes[i];
                 }
-                if(cnt>1){
-                    ok=false;
+                if (cnt > 1) {
+                    ok = false;
                     break;
                 }
-                if(cnt==1) m|=(1<<i);
+                if (cnt == 1)
+                    m |= (1 << i);
             }
 
-            if(ok) mask[x]=m;
+            if (ok) mask[x] = m;
         }
 
-        long long ones=0;
-        vector<long long> dp(1<<10,0);
-        dp[0]=1;
+        vector<long long> dp(1 << 10, 0);
+        dp[0] = 1;
 
-        for(int x:arr){
-            if(x==1){
+        int ones = 0;
+
+        for (int x : arr) {
+            if (x == 1) {
                 ones++;
                 continue;
             }
 
-            if(mask[x]==-1) continue;
+            if (mask[x] == -1) continue;
 
-            vector<long long> ndp(dp);
+            int m = mask[x];
 
-            for(int s=0;s<(1<<10);s++){
-                if((s & mask[x])==0){
-                    ndp[s|mask[x]]=(ndp[s|mask[x]]+dp[s])%MOD;
+            for (int s = (1 << 10) - 1; s >= 0; s--) {
+                if ((s & m) == 0) {
+                    dp[s | m] = (dp[s | m] + dp[s]) % MOD;
                 }
             }
-            dp.swap(ndp);
         }
 
-        long long ans=0;
-        for(int s=1;s<(1<<10);s++)
-            ans=(ans+dp[s])%MOD;
+        long long ans = 0;
+        for (int s = 1; s < (1 << 10); s++)
+            ans = (ans + dp[s]) % MOD;
 
-        long long mul=1;
-        while(ones--){
-            mul=(mul*2)%MOD;
-        }
+        long long mul = 1;
+        while (ones--)
+            mul = (mul * 2) % MOD;
 
-        ans=(ans*mul)%MOD;
-        return ans;
+        return (ans * mul) % MOD;
     }
 };
