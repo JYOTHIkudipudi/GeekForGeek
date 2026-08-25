e3db2bef601b282e4b045475caa7b71a0df782cb
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2026-08-25 21:51:33 +0530

    Create MinimumMovestoSort.cpp

---
+++ MinimumMovestoSort.cpp
@@ -0,0 +1,54 @@
+/*  Minimum Moves to Sort Permutation
+
+Given an array arr[] containing integers from 1 to n exactly once, sort the array in ascending order.
+
+In one operation, you can pick any element and move it either to the beginning or to the end of the array.
+
+Return the minimum number of operations required to sort the array.
+
+Examples:
+
+Input: arr[] = [2, 1, 3]
+Output: 1
+Explanation: Move 1 to the beginning.
+Input: arr[] = [4, 3, 1, 2]
+Output: 2
+Explanation: Move 3 to the end to get [4, 1, 2, 3]. Then move 4 to the end to get [1, 2, 3, 4].
+Constraints:
+
+arr.size() ≤ 105
+1 ≤ arr[i] ≤ arr.size()
+*/
+
+class Solution {
+  public:
+    int minMoves(vector<int>& arr) {
+        
+        int n = arr.size();
+        
+        // pos[x] = x array lo unna index
+        vector<int> pos(n + 1);
+        
+        for (int i = 0; i < n; i++) {
+            pos[arr[i]] = i;
+        }
+        
+        int current = 1;
+        int longest = 1;
+        
+        // Check consecutive numbers
+        for (int num = 2; num <= n; num++) {
+            
+            if (pos[num - 1] < pos[num]) {
+                current++;
+            } 
+            else {
+                current = 1;
+            }
+            
+            longest = max(longest, current);
+        }
+        
+        return n - longest;
+    }
+};
